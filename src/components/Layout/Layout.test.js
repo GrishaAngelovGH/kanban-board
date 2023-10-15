7940e2cdaab455f4e66f9962aefbaed518ff83eb
Grisha Angelov <grisha.angelov89@gmail.com>
2023-10-15 09:57:18 +0200

    provide props for Layout in unit test

--- src/components/Layout/Layout.test.js
+++ src/components/Layout/Layout.test.js
@@ -3,7 +3,12 @@ import { render } from "@testing-library/react"
 import Layout from "./Layout"
 
 test("should render Layout component", () => {
-  const view = render(<Layout />)
+  const view = render(
+    <Layout
+      header={<div>Header</div>}
+      body={<div>Body</div>}
+    />
+  )
 
   expect(view).toMatchSnapshot()
 })
