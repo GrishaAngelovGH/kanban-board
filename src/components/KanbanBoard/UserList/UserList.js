05e67a9fb91393d1f952a0db059865e6fc9a993c
Grisha Angelov <grisha.angelov89@gmail.com>
2023-10-24 10:36:27 +0200

    select/deselect users from UserList

--- src/components/KanbanBoard/UserList/UserList.js
+++ src/components/KanbanBoard/UserList/UserList.js
@@ -1,15 +1,26 @@
+import { useState } from "react"
+
 import ListGroup from "react-bootstrap/ListGroup"
 
 import users from "users"
 
 const User = ({ name, image }) => {
+  const [isSelected, setIsSelected] = useState(false)
+
+  const handleToggle = () => {
+    setIsSelected(!isSelected)
+  }
+
   return (
-    <div className="row align-items-center">
+    <div className="row align-items-center" onClick={handleToggle}>
       <div className="col-2">
         <img src={image} width={50} className="rounded-circle" alt="assigned-user" />
       </div>
-      <div className="col-10">
+      <div className="col-8">
         <p className="m-0 fw-bold">{name}</p>
+      </div>
+      <div className="col-2">
+        {isSelected && (<i className="bi bi-check-circle-fill text-success fs-4"></i>)}
       </div>
     </div>
   )
