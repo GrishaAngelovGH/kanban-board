867f575ca51de9e0844e49f13e541546a3eff698
Grisha Angelov <grisha.angelov89@gmail.com>
2023-11-04 14:36:47 +0200

    extract buttons in Header into separate components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,41 +4,60 @@ import DropdownButton from "react-bootstrap/DropdownButton"
 
 import "./Header.css"
 
-const Header = ({ downloadJsonHref, disabledExport, onSettingsClick, onCalendarClick, onImportKanbanBoardClick }) => {
+export const CalendarButton = ({ onClick }) => (
+  <i role="button" className="bi bi-calendar2-week text-secondary fs-4" onClick={onClick}></i>
+)
+
+export const ExchangeButton = ({ downloadJsonHref, disabledExport, onImportKanbanBoardClick }) => {
   const filename = `kanban-board-${new Date().toString().toLowerCase().split(" ").slice(0, 5).join("-")}.json`
 
   return (
-    <div className="row bg-secondary-subtle p-2 justify-content-between">
-      <div className="col col-md-3 col-lg-2">
-        <h3 className="m-0">Kanban Board</h3>
-      </div>
-      <div className="col-3 text-end">
-        <i role="button" className="bi bi-calendar2-week text-secondary fs-4" onClick={onCalendarClick}></i>
-
-        <DropdownButton
-          as={ButtonGroup}
-          variant="bg-secondary"
-          title={<i className="bi bi-filetype-json"></i>}
-          className="json-export-btn"
-        >
-          <Dropdown.Item
-            href={downloadJsonHref}
-            download={filename}
-            disabled={disabledExport}
-          >
-            <i className="bi bi-box-arrow-up text-secondary fs-5 mx-1"></i>
-            Export to JSON
-          </Dropdown.Item>
-          <Dropdown.Item onClick={onImportKanbanBoardClick}>
-            <i className="bi bi-box-arrow-down text-secondary fs-5 mx-1"></i>
-            Import from JSON
-          </Dropdown.Item>
-        </DropdownButton>
-
-        <i role="button" className="bi bi-gear text-secondary fs-4" onClick={onSettingsClick}></i>
-      </div>
-    </div>
+    <DropdownButton
+      as={ButtonGroup}
+      variant="bg-secondary"
+      title={<i className="bi bi-filetype-json"></i>}
+      className="json-export-btn"
+    >
+      <Dropdown.Item
+        href={downloadJsonHref}
+        download={filename}
+        disabled={disabledExport}
+      >
+        <i className="bi bi-box-arrow-up text-secondary fs-5 mx-1"></i>
+        Export to JSON
+      </Dropdown.Item>
+      <Dropdown.Item onClick={onImportKanbanBoardClick}>
+        <i className="bi bi-box-arrow-down text-secondary fs-5 mx-1"></i>
+        Import from JSON
+      </Dropdown.Item>
+    </DropdownButton>
   )
 }
+
+export const SettingsButton = ({ onClick }) => (
+  <i role="button" className="bi bi-gear text-secondary fs-4" onClick={onClick}></i>
+)
+
+const Header = ({
+  downloadJsonHref, disabledExport,
+  onSettingsClick, onCalendarClick, onImportKanbanBoardClick
+}) => (
+  <div className="row bg-secondary-subtle p-2 justify-content-between">
+    <div className="col col-md-3 col-lg-2">
+      <h3 className="m-0">Kanban Board</h3>
+    </div>
+    <div className="col-3 text-end">
+      <CalendarButton onClick={onCalendarClick} />
+
+      <ExchangeButton
+        downloadJsonHref={downloadJsonHref}
+        disabledExport={disabledExport}
+        onImportKanbanBoardClick={onImportKanbanBoardClick}
+      />
+
+      <SettingsButton onClick={onSettingsClick} />
+    </div>
+  </div>
+)
 
 export default Header
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,9 +1,27 @@
 import { render } from "@testing-library/react"
 
-import Header from "./Header"
+import Header, { CalendarButton, ExchangeButton, SettingsButton } from "./Header"
 
 test("should render Header component", () => {
   const view = render(<Header />)
+
+  expect(view).toMatchSnapshot()
+})
+
+test("should render CalendarButton component", () => {
+  const view = render(<CalendarButton />)
+
+  expect(view).toMatchSnapshot()
+})
+
+test("should render ExchangeButton component", () => {
+  const view = render(<ExchangeButton />)
+
+  expect(view).toMatchSnapshot()
+})
+
+test("should render SettingsButton component", () => {
+  const view = render(<SettingsButton />)
 
   expect(view).toMatchSnapshot()
 })
