9e1bdb789415858713bd2195ffa10a23916d32df
Grisha Angelov <grisha.angelov89@gmail.com>
2023-12-07 09:39:11 +0200

    add createUser() function to persistentUserRepository

--- src/persistent/persistentUserRepository.js
+++ src/persistent/persistentUserRepository.js
@@ -1,8 +1,17 @@
 const persistentUserRepository = {
+  createUser: (image, name) => {
+    const users = JSON.parse(localStorage.getItem("users"))
+    const id = Math.random().toString().slice(2)
+
+    const newUsers = {
+      ...users,
+      [id]: { id, name, image }
+    }
+
+    localStorage.setItem("users", JSON.stringify(newUsers))
+  },
   getUsers: () => {
     const users = JSON.parse(localStorage.getItem("users"))
-
-    if (!users || !Object.values(users).length) return []
 
     return (users && Object.values(users)) || []
   },
