3e4221bc02e43aa606600545fe477101483a4b53
Grisha Angelov <grisha.angelov89@gmail.com>
2023-12-10 10:04:29 +0200

    hide title and priority when task is used as a template in TaskModal

--- src/components/KanbanBoard/modals/TaskModals/TaskModal/TaskModal.js
+++ src/components/KanbanBoard/modals/TaskModals/TaskModal/TaskModal.js
@@ -1,12 +1,14 @@
 import Modal from "components/Modal"
 import Form from "react-bootstrap/Form"
+import FormCheck from "react-bootstrap/FormCheck"
 import Editor from "react-simple-wysiwyg"
 
 import "./TaskModal.css"
 
 const TaskModal = ({
-  show, modalTitle, title, description, priority,
-  onTitleChange, onDescriptionChange, onPriorityChange, onClose, onConfirm
+  show, modalTitle, title, description, priority, isTemplate,
+  onTitleChange, onDescriptionChange, onPriorityChange,
+  onTemplateChange, onClose, onConfirm
 }) => (
   <Modal
     show={show}
@@ -17,21 +19,36 @@ const TaskModal = ({
     body={
       <div className="row">
         <div className="col-md-12">
-          <Form.Control
-            value={title}
-            placeholder="Title"
+          <FormCheck
+            checked={isTemplate}
+            label="Use as a template"
             className="mb-3"
-            onChange={onTitleChange}
+            onChange={onTemplateChange}
           />
+
+          {
+            !isTemplate && (
+              <Form.Control
+                value={title}
+                placeholder="Title"
+                className="mb-3"
+                onChange={onTitleChange}
+              />
+            )
+          }
 
           <Editor value={description} onChange={onDescriptionChange} />
 
-          <Form.Select value={priority} onChange={onPriorityChange} className="mt-3">
-            <option value="" className="text-secondary fw-bold">No Priority</option>
-            <option value="low" className="text-warning fw-bold">Low Priority</option>
-            <option value="medium" className="text-success fw-bold">Medium Priority</option>
-            <option value="high" className="text-danger fw-bold">High Priority</option>
-          </Form.Select>
+          {
+            !isTemplate && (
+              <Form.Select value={priority} onChange={onPriorityChange} className="mt-3">
+                <option value="" className="text-secondary fw-bold">No Priority</option>
+                <option value="low" className="text-warning fw-bold">Low Priority</option>
+                <option value="medium" className="text-success fw-bold">Medium Priority</option>
+                <option value="high" className="text-danger fw-bold">High Priority</option>
+              </Form.Select>
+            )
+          }
         </div>
       </div>
     }
--- src/components/KanbanBoard/modals/TaskModals/TaskModal/TaskModal.test.js
+++ src/components/KanbanBoard/modals/TaskModals/TaskModal/TaskModal.test.js
@@ -20,3 +20,25 @@ test("should render TaskModal component", () => {
 
   expect(view).toMatchSnapshot()
 })
+
+test("should render TaskModal component as a template", () => {
+  const onUpdate = jest.fn()
+  const onTemplateChange = jest.fn()
+
+  const view = render(
+    <TaskModal
+      show
+      modalTitle="Create New Task"
+      title="Title Task"
+      description="Title Description"
+      priority="medium"
+      isTemplate={true}
+      onTitleChange={onUpdate}
+      onDescriptionChange={onUpdate}
+      onPriorityChange={onUpdate}
+      onTemplateChange={onTemplateChange}
+    />
+  )
+
+  expect(view).toMatchSnapshot()
+})
