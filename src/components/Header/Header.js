ddecab609cad82a4229b3dc91b1ce68ad05ee9cb
Grisha Angelov <grisha.angelov89@gmail.com>
2023-12-21 13:04:21 +0200

    redesign Header's menu to provide icon and label for each item

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,81 +1,51 @@
-import Button from "react-bootstrap/Button"
 import ButtonGroup from "react-bootstrap/ButtonGroup"
 import Dropdown from "react-bootstrap/Dropdown"
 import DropdownButton from "react-bootstrap/DropdownButton"
 
-const ExchangeButton = ({ downloadJsonHref, disabledExport, onImportKanbanBoardClick }) => {
-  const filename = `kanban-board-${new Date().toString().toLowerCase().split(" ").slice(0, 5).join("-")}.json`
-
-  return (
-    <DropdownButton
-      as={ButtonGroup}
-      variant="outline-secondary"
-      title={<i className="bi bi-filetype-json"></i>}
-    >
-      <Dropdown.Item
-        href={downloadJsonHref}
-        download={filename}
-        disabled={disabledExport}
-      >
-        <i className="bi bi-box-arrow-up text-secondary fs-5 mx-1"></i>
-        Export to JSON
-      </Dropdown.Item>
-      <Dropdown.Item onClick={onImportKanbanBoardClick}>
-        <i className="bi bi-box-arrow-down text-secondary fs-5 mx-1"></i>
-        Import from JSON
-      </Dropdown.Item>
-    </DropdownButton>
-  )
-}
-
 const Header = ({
   downloadJsonHref, disabledExport,
   onUsersClick, onSettingsClick, onCalendarClick, onImportKanbanBoardClick
-}) => (
-  <div className="row bg-secondary-subtle p-2 justify-content-between align-items-center">
-    <div className="col-4 col-md-4 col-lg-3">
-      <h3 className="m-0">Kanban Board</h3>
+}) => {
+  const filename = `kanban-board-${new Date().toString().toLowerCase().split(" ").slice(0, 5).join("-")}.json`
+
+  const menuItems = [
+    {
+      label: "Toggle Fullscreen",
+      icon: "bi bi-arrows-fullscreen",
+      onClick: () => {
+        const isFullScreen = window.document.fullscreenElement
+
+        isFullScreen ?
+          window.document.exitFullscreen() :
+          window.document.documentElement.requestFullscreen()
+      }
+    },
+    { label: "Toggle Calendar", icon: "bi bi-calendar2-week", onClick: onCalendarClick },
+    { label: "Export to JSON", icon: "bi bi-box-arrow-up", href: downloadJsonHref, download: filename, disabled: disabledExport },
+    { label: "Import from JSON", icon: "bi bi-box-arrow-down", onClick: onImportKanbanBoardClick },
+    { label: "Manage Users", icon: "bi bi-people", onClick: onUsersClick },
+    { label: "Settings", icon: "bi bi-gear", onClick: onSettingsClick }
+  ]
+
+  return (
+    <div className="row bg-secondary-subtle p-2 justify-content-between align-items-center">
+      <div className="col-4 col-md-4 col-lg-3">
+        <h3 className="m-0">Kanban Board</h3>
+      </div>
+      <div className="col-8 col-md-4 col-lg-3 text-end">
+        <DropdownButton as={ButtonGroup} size="sm" variant="light" title={<i className="bi bi-grid-fill text-primary"></i>}>
+          {
+            menuItems.map((v, i) => (
+              <Dropdown.Item key={i} {...v}>
+                <i className={v.icon}></i>
+                <span className="mx-2">{v.label}</span>
+              </Dropdown.Item>
+            ))
+          }
+        </DropdownButton>
+      </div>
     </div>
-    <div className="col-8 col-md-4 col-lg-3 text-end">
-      <ButtonGroup>
-        <Button
-          variant="outline-secondary"
-          className="bi bi-arrows-fullscreen"
-          onClick={() => {
-            const isFullScreen = window.document.fullscreenElement
-
-            isFullScreen ?
-              window.document.exitFullscreen() :
-              window.document.documentElement.requestFullscreen()
-          }}
-        />
-
-        <Button
-          variant="outline-secondary"
-          className="bi bi-calendar2-week"
-          onClick={onCalendarClick}
-        />
-
-        <ExchangeButton
-          downloadJsonHref={downloadJsonHref}
-          disabledExport={disabledExport}
-          onImportKanbanBoardClick={onImportKanbanBoardClick}
-        />
-
-        <Button
-          variant="outline-secondary"
-          className="bi bi-people"
-          onClick={onUsersClick}
-        />
-
-        <Button
-          variant="outline-secondary"
-          className="bi bi-gear"
-          onClick={onSettingsClick}
-        />
-      </ButtonGroup>
-    </div>
-  </div>
-)
+  )
+}
 
 export default Header
