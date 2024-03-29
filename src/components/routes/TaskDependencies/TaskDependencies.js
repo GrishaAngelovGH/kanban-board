becb0b0bb46953e25cc29ebd3cd62195afe16bfa
Grisha Angelov <grisha.angelov89@gmail.com>
2024-03-29 14:51:23 +0200

    visualize all tasks within the columns and provide the capability to add a given task as a dependency to the selected task

--- src/components/routes/TaskDependencies/TaskDependencies.js
+++ src/components/routes/TaskDependencies/TaskDependencies.js
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom"
 
+import Badge from "react-bootstrap/Badge"
+import Button from "react-bootstrap/Button"
+import ListGroup from "react-bootstrap/ListGroup"
+
 import boardRepository from "persistent/persistentKanbanBoardRepository"
 
 import useBackgroundImage from "hooks/useBackgroundImage"
+
+import "./TaskDependencies.css"
 
 const TaskDependencies = ({ taskId, columnId }) => {
   const backgroundImage = useBackgroundImage()
@@ -25,6 +31,43 @@ const TaskDependencies = ({ taskId, columnId }) => {
         <div className="row g-0 mt-3 justify-content-center">
           <div className="col-6 bg-light rounded">
             <h3 className="m-0 text-center">{task.title}</h3>
+          </div>
+        </div>
+
+        <div className="row g-0 mt-3 justify-content-around overflow-auto task-dependencies">
+          <div className="col-4">
+            <ListGroup>
+              <ListGroup.Item className="bg-secondary-subtle text-secondary fw-bold text-center">
+                All columns with tasks
+              </ListGroup.Item>
+              {
+                columns
+                  .filter(col => col.items.length)
+                  .map(col => (
+                    <ListGroup.Item key={col.id}>
+                      <Badge className="mb-3">{col.title}</Badge>
+                      {
+                        col.items.map(v => (
+                          <ListGroup.Item key={v.id} className="border border-3 rounded mb-3">
+                            <p>{v.title}</p>
+                            <Button
+                              size="sm"
+                              variant="outline-primary"
+                              className="w-100"
+                              disabled={v.id === taskId}
+                              onClick={(() => {
+                                boardRepository.addDependencyTask(taskId, columnId, v.id)
+                              })}
+                            >
+                              Add as a dependency
+                            </Button>
+                          </ListGroup.Item>
+                        ))
+                      }
+                    </ListGroup.Item>
+                  ))
+              }
+            </ListGroup>
           </div>
         </div>
       </div>
