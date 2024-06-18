9389bcf473810c649892d7c5a7e16156e5a70921
Grisha Angelov <grisha.angelov89@gmail.com>
2024-06-18 13:24:45 +0300

    remove the duplication of buttons in TaskLimitInput

--- src/components/KanbanBoard/Column/TaskLimitInput/TaskLimitInput.js
+++ src/components/KanbanBoard/Column/TaskLimitInput/TaskLimitInput.js
@@ -2,27 +2,34 @@ import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
 import InputGroup from "react-bootstrap/InputGroup"
 
-const TaskLimitInput = ({ currentLimit, disabledConfirm, onChange, onCancel, onConfirm }) => (
-  <InputGroup>
-    <Form.Control
-      type="number"
-      min={0}
-      value={currentLimit}
-      onChange={onChange}
-    />
-    <Button
-      variant="secondary"
-      onClick={onCancel}
-    >
-      Cancel
-    </Button>
-    <Button
-      variant="success"
-      disabled={disabledConfirm}
-      onClick={onConfirm}>
-      OK
-    </Button>
-  </InputGroup>
-)
+const TaskLimitInput = ({ currentLimit, disabledConfirm, onChange, onCancel, onConfirm }) => {
+  const buttons = [
+    { variant: "secondary", disabled: false, onClick: onCancel, label: "Cancel" },
+    { variant: "success", disabled: disabledConfirm, onClick: onConfirm, label: "OK" }
+  ]
+
+  return (
+    <InputGroup>
+      <Form.Control
+        type="number"
+        min={0}
+        value={currentLimit}
+        onChange={onChange}
+      />
+      {
+        buttons.map((v, i) => (
+          <Button
+            key={i}
+            variant={v.variant}
+            disabled={v.disabled}
+            onClick={v.onClick}
+          >
+            {v.label}
+          </Button>
+        ))
+      }
+    </InputGroup>
+  )
+}
 
 export default TaskLimitInput 
