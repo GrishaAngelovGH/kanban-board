b598f1eb80877ef33792ada53744f8ac05a835cb
Grisha Angelov <grisha.angelov89@gmail.com>
2024-06-18 13:38:12 +0300

    improve the readability in Avatar

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,15 +1,21 @@
 import "./Avatar.css"
 
 const Avatar = ({ user: { image, name }, size = 50 }) => {
-  return image.length > 0 ?
-    <img src={image} width={size} className="rounded-circle m-1" alt="user" /> :
-    <div
-      className="rounded-circle border border-secondary m-1 d-flex justify-content-center align-items-center fw-bold avatar"
-      style={{ width: size, height: size }}
-    >
-      {name.split(" ").map(v => v[0]).join("")}
-    </div>
-}
+  const hasImage = image.length > 0
+  const nameInitials = name.split(" ").map(v => v[0]).join("")
 
+  return hasImage ?
+    (
+      <img src={image} width={size} className="rounded-circle m-1" alt="user" />
+    ) :
+    (
+      <div
+        className="rounded-circle border border-secondary m-1 d-flex justify-content-center align-items-center fw-bold avatar"
+        style={{ width: size, height: size }}
+      >
+        {nameInitials}
+      </div>
+    )
+}
 
 export default Avatar
