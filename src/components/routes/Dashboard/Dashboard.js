cc964dd789b003fe335bb3693e9a0a5af2ca6797
Grisha Angelov <grisha.angelov89@gmail.com>
2024-06-19 10:12:28 +0300

    integrate the Page component into Dashboard

--- src/components/routes/Dashboard/Dashboard.js
+++ src/components/routes/Dashboard/Dashboard.js
@@ -1,29 +1,20 @@
-import { Link } from "react-router-dom"
-
-import useBackgroundImage from "hooks/useBackgroundImage"
+import Page from "components/routes/Page"
 
 const Dashboard = () => {
-  const backgroundImage = useBackgroundImage()
+  const description = (
+    <div className="col-10">
+      <h3 className="m-0 text-center">Dashboard</h3>
+    </div>
+  )
 
   return (
-    <div className="row g-0 vh-100" style={{ backgroundImage: backgroundImage ? `url(${backgroundImage})` : "none", backgroundSize: "cover" }}>
-      <div className="col-12">
-        <div className="row g-0 bg-light p-2 shadow align-items-center">
-          <div className="col-2">
-            <Link to="/" className="btn btn-light bi bi-arrow-left border"></Link>
-          </div>
-          <div className="col-8">
-            <h3 className="m-0 text-center">Dashboard</h3>
-          </div>
-        </div>
-
-        <div className="row g-0 mt-3 justify-content-center overflow-auto tasks">
-          <div className="col-10">
-            Content
-          </div>
+    <Page description={description}>
+      <div className="row g-0 mt-3 justify-content-center overflow-auto tasks">
+        <div className="col-10">
+          Content
         </div>
       </div>
-    </div>
+    </Page>
   )
 }
 
