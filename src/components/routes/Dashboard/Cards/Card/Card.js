562cc002152dec1a2bbda291ed1b87d8533774f8
Grisha Angelov <grisha.angelov89@gmail.com>
2024-06-26 14:06:03 +0300

    provide Header and Body for the Card component

--- src/components/routes/Dashboard/Cards/Card/Card.js
+++ src/components/routes/Dashboard/Cards/Card/Card.js
@@ -1,17 +1,25 @@
 const Card = ({ title, children }) => (
   <div style={{ height: 350 }} className="col col-md-5 bg-white rounded shadow">
-    <div className="d-flex justify-content-between align-items-center">
-      <p className="m-0 ms-2">{title}</p>
-      <div className="d-flex align-items-center">
-        <i className="bi bi-star fs-4 text-warning"></i>
-        <i className="bi bi-x fs-2"></i>
-      </div>
-    </div>
-    <div className="d-flex justify-content-center">
-      {
-        children
-      }
-    </div>
+    <Card.Header title={title} />
+
+    <Card.Body>
+      {children}
+    </Card.Body>
+  </div>
+)
+
+Card.Header = ({ title }) => (
+  <div className="d-flex justify-content-between align-items-center">
+    <p className="m-0">{title}</p>
+    <i className="bi bi-star fs-4 text-warning"></i>
+  </div>
+)
+
+Card.Body = ({ children }) => (
+  <div className="d-flex justify-content-center">
+    {
+      children
+    }
   </div>
 )
 
