2e875ad7303fdd0b0a53b197651bfad95e813485
Grisha Angelov <grisha.angelov89@gmail.com>
2024-07-02 15:18:45 +0300

    update bootstrap, bootstrap-icons and react-bootstrap dependencies

--- src/components/routes/Dashboard/Toolbar/Toolbar.js
+++ src/components/routes/Dashboard/Toolbar/Toolbar.js
@@ -4,20 +4,18 @@ const Toolbar = ({ items, onClick }) => (
       <p className="text-secondary text-center">Click on any item to add the corresponding card to the dashboard</p>
       <div className="row flex-column flex-md-row g-1 justify-content-evenly">
         {
-          items
-            .map((v, i) => (
-              <div
-                data-testid="toolbar-item"
-                key={i}
-                className="col-md-2 bg-primary text-white text-center rounded"
-                onClick={() => { onClick(v.id) }}
-              >
-                {v.title}
-              </div>
-            ))
+          items.map((v, i) => (
+            <div
+              key={i}
+              data-testid="toolbar-item"
+              className="col-md-2 bg-primary text-white text-center rounded"
+              onClick={() => { onClick(v.id) }}
+            >
+              {v.title}
+            </div>
+          ))
         }
       </div>
-
     </div>
   </div>
 )
